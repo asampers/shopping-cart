d15390ee50d2897defac6cae3a5b4fad6e3070f3
nanarama <annasampers@gmail.com>
2023-09-19 16:22:49 -0500

    Add HeroIcons

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import {ShoppingBagIcon} from '@heroicons/react/24/solid'
 import './App.css'
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
+          <ShoppingBagIcon />
           count is {count}
         </button>
         <p>
@@ -18,6 +20,8 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+
+
     </>
   )
 }
