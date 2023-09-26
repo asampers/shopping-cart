f4df2cb028bbb15b7dfa48e470b2492d399ba00d
nanarama <annasampers@gmail.com>
2023-09-26 14:38:38 -0500

    More styling for CheckoutCard

--- src/components/CheckoutCard.jsx
+++ src/components/CheckoutCard.jsx
@@ -5,6 +5,7 @@ import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import { XMarkIcon, TrashIcon } from "@heroicons/react/20/solid"
+import { ArrowDownOnSquareStackIcon } from '@heroicons/react/24/solid';
 
 export default function CheckoutCard({products}) {
   const subTotal = () => {
@@ -19,9 +20,12 @@ export default function CheckoutCard({products}) {
     <Card style={{width: '25rem'}}>
       <Card.Header className="d-flex align-items-center justify-content-between">
         Your Cart:
-        <XMarkIcon width="20px"/>
+        <Button size="sm" variant="outline-dark">
+          <XMarkIcon width="20px"/>
+        </Button>  
       </Card.Header>
       <Card.Body>
+        {products.length == 0 && <h6><i>There are no items.</i></h6>}
         <ListGroup variant="flush">
             {products.map((prod) => (
               <ListGroup.Item key={prod.id}>
@@ -40,7 +44,7 @@ export default function CheckoutCard({products}) {
         </ListGroup>
         <hr style={{margin: "0 0 0.5rem 0"}}></hr>
         <Card.Text className="text-end">subtotal: ${subTotal()}</Card.Text>
-        <Button className="m-auto" variant="primary">CHECKOUT</Button>
+        <Button style={{marginLeft: "35%"}} variant="success" disabled={products.length == 0}>CHECKOUT</Button>
       </Card.Body>
     </Card>
   );
