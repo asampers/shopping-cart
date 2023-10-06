45c5a9832426f05ba32ef85cc672afdae85c9a5a
nanarama <annasampers@gmail.com>
2023-10-06 13:16:15 -0500

    Change About links to open on new tab

--- src/routes/About.jsx
+++ src/routes/About.jsx
@@ -11,8 +11,8 @@ export default function About() {
           <Card.Text>
             This app was made by Anna Sampers. Anna learned to code from The Odin Project.
           </Card.Text>
-          <a href='https://www.theodinproject.com/lessons/react-new-shopping-cart' className="btn btn-outline-secondary m-2">Click here for project instructions.</a>
-          <a href='https://github.com/asampers/shopping-cart' className="btn btn-outline-secondary">Click here for Anna Sampers&apos; Github.</a>
+          <a href='https://www.theodinproject.com/lessons/react-new-shopping-cart' target="_blank" rel="noreferrer" className="btn btn-outline-secondary m-2">Click here for project instructions.</a>
+          <a href='https://github.com/asampers/shopping-cart' target="_blank" rel="noreferrer" className="btn btn-outline-secondary">Click here for Anna Sampers&apos; Github.</a>
         </Card.Body>
       </Card>
     </Container>
